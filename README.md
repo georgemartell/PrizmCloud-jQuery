aaf4fef88b4fb4c09b69e9b70093b2b5792594c4
Prizm Cloud <plugins@prizmcloud.com>
2013-08-22 12:15:56 -0400

    Adding new slideshow feature

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 ### Prizm Cloud Document Viewer jQuery Plugin
 
-jQuery plugin allows you to display the Prizm Cloud Document Viewer with clickable thumbnails which update the base viewer with the new document dynamically. Multiple viewers can be on the same page. Prizm Cloud is a cloud based document viewer, so you can display over three hundred different file types on your website, while the documents stay on your server without additional installation of software or hardware.
+jQuery plugin allows you to display the Prizm Cloud Document Viewer with clickable thumbnails which update the base viewer with the new document dynamically. Multiple viewers can be on the same page. Prizm Cloud is a cloud based document viewer, so you can display over three hundred different file types on your website, while the documents stay on your server without additional installation of software or hardware. [Full Documentation](http://www.prizmcloud.com/plugins/documentation/jquery.html)
 
-![PrizmCloud Visual Example](PrizmCloud_Demo_for_Js.png "Prizm Cloud Visual Example")
+![Prizm Cloud Visual Example](PrizmCloud_Demo_for_Js.png "Prizm Cloud Visual Example")
 
-Here is a [demo](http://prizmcloud.accusoft.com/demo.html) of how Prizm Cloud works. There is a [jsFiddle](http://jsfiddle.net/accusoft/m2CWP/) so you can see the plugin in action!
+Here is a [demo](http://prizmcloud.com/demo.html) of how Prizm Cloud works. There is a [jsFiddle](http://jsfiddle.net/accusoft/m2CWP/) so you can see the plugin in action!
 
 ###Prerequisites
 
-You will need to [Sign Up](http://prizmcloud.accusoft.com/register.html) for a Prizm Cloud account. Don't worry there is a free account.
+You will need to [Sign Up](http://prizmcloud.com/register.html) for a Prizm Cloud account. Don't worry there is a free account.
 
 Your **html** should have the following
 
 * An outer wrapper element with a unique id
 	```
-	<div id="prizmcloud-container">...</div>
+	<div id="prizm-cloud-container">...</div>
 	```
 * Within the outer wrapper, a wrapper for the thumbnails
 	```
@@ -22,21 +22,29 @@ Your **html** should have the following
 	```
 * Within the outer wrapper, a wrapper for the Document Viewer
 	```
-	<div class="prizmcloud-viewer" id="prizmcloud-viewer">...</div>
+	<div class="prizm-cloud-viewer" id="prizm-cloud-viewer">...</div>
 	```
 
 ###Javascript Options
 
-* **type**: is the viewer type (either flash or html5)
-* **doc_url**: is the document url
-* **vwidth**: is the viewer width in px (int)
-* **vheight**: is the viewer height in px (int)
+* **type**: is the viewer type (either flash, html5, or slideshow)
+* **viewerwidth**: is the viewer width in px (int)
+* **viewerheight**: is the viewer height in px (int)
 * **print_button**: options 'Yes' or 'No'
 * **toolbar_color**: hex color, no '#' for example "CCCCCC"
-* **cache**: 'Yes' or 'No'
 * **documents_container**: container that holds the thumbnails, can be a class or id
 * **viewer_container**: the main document viewer container, can be a class or id
 
+slideshow options
+
+* **animtype**:  animation type (slide or fade)
+* **animduration**: animation duration, # in milliseconds (time animation takes to complete)
+* **animspeed**: animation speed, # in milliseconds (time in-between slides)
+* **automatic**: yes or no, start the slideshow automatically?
+* **showcontrols**: yes or no, show the slideshow controls?
+* **centercontrols**: yes or no, center the slideshow controls?
+* **keyboardnav**: yes or no, allow keyboard navigation?
+* **hoverpause**: yes or no, pause on mouse hover?
 
 ##jQuery
 
@@ -71,15 +79,15 @@ or
 Complete html example:
 
 ```
-<div id="prizmcloud-container">
-    <div class="prizmcloud-viewer" id="prizmcloud-viewer">
-        <iframe id="pc_viewer_box" width="460" height="650" frameborder="0" src="http://connect.ajaxdocumentviewer.com/?key=03232898832&viewertype=html5&document=http://www.accusoft.com/docs/prizm-cloud-flash-vs-html5.pdf&viewerheight=600&viewerwidth=440&printButton=Yes&toolbarColor=CCCCCC&cache=yes" seamless></iframe>
+<div id="prizm-cloud-container">
+    <div class="prizm-cloud-viewer" id="prizm-cloud-viewer">
+        <iframe id="pc_viewer_box" width="460" height="650" frameborder="0" src="http://connect.ajaxdocumentviewer.com/?key=03232898832&viewertype=html5&document=http://www.accusoft.com/docs/prizm-cloud-flash-vs-html5.pdf&viewerheight=600&viewerwidth=440&printButton=Yes&toolbarColor=CCCCCC" seamless></iframe>
     </div>
     <div class="documents-for-switching" id="documents-for-switching"> 
         <a href="http://www.accusoft.com/docs/prizm-cloud-flash-vs-html5.pdf" data-doc-link="http://www.accusoft.com/docs/prizm-cloud-flash-vs-html5.pdf" class="doc-link active">
-			<img src="http://prizmcloud.accusoft.com/img/pc-demo-pdf-thumb-1.gif" /></a>
+			<img src="http://prizmcloud.com/img/pc-demo-pdf-thumb-1.gif" /></a>
 		<a href="http://www.cdc.gov/phpr/documents/11_225700_A_Zombie_Final.pdf" data-doc-link="http://www.cdc.gov/phpr/documents/11_225700_A_Zombie_Final.pdf" class="doc-link">
-			<img src="http://prizmcloud.accusoft.com/img/pc-demo-pdf-thumb-3.gif" /></a>
+			<img src="http://prizmcloud.com/img/pc-demo-pdf-thumb-3.gif" /></a>
     </div>
 </div>
 ```
@@ -94,11 +102,20 @@ $('selector').prizmcloud();
 In jQuery document ready event
 ```
 jQuery(document).ready(function($) {
-    $('#prizmcloud-container').prizmcloud({
-        vheight: 400,
-        vwidth: 400,
+    $('#prizm-cloud-container').prizmcloud({
+        viewerheight: 400,
+        viewerwidth: 400,
         type: 'flash',
         print_button: 'No'
+    });
+	$('#prizm-cloud-container-2').prizmcloud({
+        viewerheight: 640,
+        viewerwidth: 440,
+        type: 'slideshow',
+        slideshow: {
+            animtype: "fade",
+			automatic: "no"
+        }
     });
 });
 ```
--- jquery.prizmcloud.js
+++ jquery.prizmcloud.js
@@ -1,12 +1,13 @@
 /*
  * jQuery plugin to display Prizm Cloud Document Viewer
  * with clickable thumbnails that dynamically update viewer
- * with new document
+ * with new document, with a slideshow feature
  *
- * Plugin url: www.prizmcloud.accusoft.com
+ * Plugin url: www.prizmcloud.com
  * Author: Accusoft
  * Author url: www.accusoft.com
- *
+ * Documentaion: http://www.prizmcloud.com/plugins/documentation/jquery.html
+ * 
  * Structure example
  * <div with "your container id">
  *     <div "container for document links">
@@ -14,52 +15,62 @@
  *    </div>
  *    <div "container for viewer"></div>
  * </div>
- * script jQuery("your container id").prizmcloud({ key: "", vwidth: 650, vheight: 700 });
+ * script jQuery("your container id").prizmcloud({ key: "11111111", viewerwidth: 650, viewerheight: 700 });
  */
 (function ($) {
     $.fn.prizmcloud = function (options) {
 
         // extend default options.
-        var opts = $.extend({}, $.fn.prizmcloud.defaults, options);
-        var base_url = "http://connect.ajaxdocumentviewer.com/?key=";
-		var integration = "jQuery";
+        var opts = $.extend(true,{}, $.fn.prizmcloud.defaults, options);
+        var base_url = "//connect.ajaxdocumentviewer.com/?key=";
 
         bindClicks(this);
-
+        
         function bindClicks(obj) {
             var $docs_container = $(opts.documents_container);
+            obj.children($docs_container).addClass('prizm-cloud-viewer-active');
             obj.children($docs_container).children('.doc-link').each(function () {
 
                 $(this).on('click', function (e) {
                     e.preventDefault();
                     var $link = $(this),
                         docurl = "&document=",
-                        rand_num = Math.floor(Math.random() * 11);
+                        rand_num = Math.floor(Math.random() * 11),
+                        src_url = base_url + opts.key;
                     // don't reload active document
                     if(!$link.hasClass('active'))
                     {
                         $link.parent().find('.doc-link').removeClass('active');
                         $link.addClass('active');
-                        if($link.data('doc-link') != undefined)
+
+                        if($link.data('doc-link') !== undefined) 
                         {
                             docurl += $link.data('doc-link');
                         }
-                        else if($link.attr('href') != undefined)
+                        else if($link.attr('href') !== undefined) 
                         {
                             // test for href as backup
                             docurl += $link.attr('href');
                         }
-                        var src_url = base_url + opts.key + docurl + "&viewerheight=" + opts.vheight + "&viewerwidth=" + opts.vwidth + "&viewertype=" + opts.type + "&printButton=" + opts.print_button + "&toolbarColor=" + opts.toolbar_color + "&cache=" + opts.cache + "&integration=" + $integration;
-
+                        src_url += docurl + "&viewerheight=" + opts.viewerheight + "&viewerwidth=" + opts.viewerwidth + "&viewertype=" + opts.type;
+                        if(opts.type.toLowerCase() == "slideshow") 
+                        {
+                            for(var skey in opts.slideshow)
+                                src_url += "&" + skey + "=" + opts.slideshow[skey];
+                        } 
+                        else 
+                        {
+                            src_url += "&printButton=" + opts.print_button + "&toolbarColor=" + opts.toolbar_color;
+                        }
                         var viewer_iframe = $('<iframe></iframe>', {
                             'id': "prizmcloud-iframe-" + rand_num,
-                            'width': (opts.vwidth + 20),
-                            'height': (opts.vheight + 20),
+                            'width': (opts.viewerwidth + 20),
+                            'height': (opts.viewerheight + 30),
                             'src': src_url,
                             'frameborder': 0,
                             'seamless': 'seamless'
                         });
-
+                        
                         obj.children(opts.viewer_container).html(viewer_iframe);
                     }
                     return false;
@@ -69,23 +80,24 @@
     };
     $.fn.prizmcloud.defaults = {
         // These are the defaults.
-        key: "03232898832",      // your PrizmCloud Key, this is required
-        type: "html5",           // html5 or flash
-        vwidth: 600,             // viewer width
-        vheight: 600,            // viewer height
-        print_button: "Yes",     // Yes or No
-        toolbar_color: "CCCCCC", // hex color
+        key: "03232898832",         // your Prizm Cloud Key, this is required
+        type: "html5",              // html5, flash or slideshow
+        viewerwidth: 500,           // viewer width
+        viewerheight: 650,          // viewer height
+        print_button: "Yes",        // Yes or No
+        toolbar_color: "CCCCCC",    // hex color
         documents_container: "#documents-for-switching", // can be a class or id
-        viewer_container: "#prizmcloud-viewer",          // can be a class or id
-        cache: "yes"  // yes or no
+        viewer_container: "#prizm-cloud-viewer",         // can be a class or id
+        // slideshow options below
+        slideshow: {
+            animtype: "slide",      // animation type: slide or fade
+            animduration: 450,      // # in milliseconds 
+            animspeed: 4000,        // # in milliseconds
+            automatic: "yes",       // Yes or No, automatically start the slideshow?
+            showcontrols: "yes",    // Yes or no, show the slideshow navigation controls?
+            centercontrols: "yes",  // Yes or No, center the slideshow controls?
+            keyboardnav: "yes",     // Yes or No, allow keyboard navigation?
+            hoverpause: "yes"       // Yes or No, Pause slideshow on mouse hover?
+        }
     };
 }(jQuery));
-jQuery(document).ready(function($) {
-    $('#prizmcloud-container').prizmcloud({
-        vheight: 400,
-        vwidth: 400,
-        type: 'flash',
-        print_button: 'No'
-    });
-    $('#prizmcloud-container-2').prizmcloud();
-});
